794dc5f22b582132acf1f3670dc9c779a4539ce2
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2024-08-03 11:01:03 -0700

    Create script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+/*
+let js = 'amazing';
+// if(js === 'amazing') alert('JavaScript is FUN!'); 
+
+// 40+8+23-10
+console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"
+let first = 'jonas';
+let firstNamePerson
+let first_name
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let Person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true;
+console.log("javascriptIsFun");
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+cosole.log(year);
+cosole.log(typeof year);
+
+year = 1991;
+cosole.log(typeof year);
+
+cosole.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+//const varibale can't be changed
+const birthYear = 1991;
+birthYear = 1990;
